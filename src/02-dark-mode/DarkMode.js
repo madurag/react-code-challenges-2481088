f337fefa1c060885fbee96ebe68a3b1ccb309458
mint <madurag@gmail.com>
2023-01-03 11:36:49 -0500

    DarkMode Challange Completed

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 export default function DarkMode () {
+  const [themeName, setTheme] = useState('');
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${themeName}`}>
+        <button className='dark-mode-button' onClick={() => { setTheme('dark-mode') } }>Dark Mode</button>
+        <button className='light-mode-button' onClick={() => { setTheme('') } }>Light Mode</button>
     </div>
   )
 }
