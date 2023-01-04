411070ba872a92d1a055778f869500df3cf34ce6
mint <madurag@gmail.com>
2023-01-04 12:45:47 -0500

    AddWindowEvent challenge completed

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+
 export default function WindowEvent () {
+
+  useEffect(() => {
+    const doubleClickHandler = () => {
+      alert('User Double Clicked!');
+    }
+
+    window.addEventListener('dblclick', doubleClickHandler);
+    
+    return () => window.removeEventListener('dblclick', doubleClickHandler);
+
+    }, [])
+
   return (
     <h2>Window event active</h2>
   )
