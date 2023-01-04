9cd8fdd6981bfefee11e1ee9f43bf1356b2bbaed
madurag <93881067+madurag@users.noreply.github.com>
2023-01-04 09:22:25 -0500

    DocPics_Challenge_Completed

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+  // 'https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg'
+
+  let [dogPhoto, setDogPhoto] = useState(null);
+
+  async function fetchNextDog() {
+    const handler = await fetch('https://dog.ceo/api/breeds/image/random');
+    setDogPhoto(await handler.json());
+  }
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={ (dogPhoto) ? dogPhoto.message : 'https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' } alt='' />
+      <button onClick={ () => fetchNextDog() }>🐶</button>
     </div>
   )
 }
