94d7d6535d1f1f44fbf18abafc900c27e77aa9c2
mint <madurag@gmail.com>
2023-01-05 07:06:08 -0500

    SimpleCalculator_Challenge_Completed with second approach

--- src/09-simple-calculator/SimpleCalculator.js
+++ src/09-simple-calculator/SimpleCalculator.js
@@ -1,88 +1,67 @@
-import { useReducer, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 
-const initialOps = [
-  {
-  type: "ADD", 
-    numbers: [],
-    result: 0,
-    completed: false 
-  },
-  {
-    type: "SUBSTRACT",
-    numbers: [],
-    result: 0,
-    completed: false
-  }
-]
+const initialState = {
+  number1: 0,
+  number2: 0,
+  result: 0
+}
 
 function reducer (state, action) {
-  let sum = 0;
-  console.log(action);
-
-  switch(action.type) {
-    case "ADD" : {
-        action.numbers.map((number) => sum += Number(number));
-        break;
-    }
-    case "SUBSTRACT" : {
-      action.numbers.map((number) => sum = (sum === 0) ? Number(number) : sum -= Number(number));
-        break;
-    }
-    case "CLEAR" : {
-      sum = 0;
+  switch (action.type) {
+    case "NUMBER_1" : {
+      state.number1 = action.num;
       break;
     }
-    default : {
-      throw new Error("Unknown Operation");      
+    case "NUMBER_2" : {
+      state.number2 = action.num;
+      break;
+    }
+    case "ADD" : {
+      state.result = state.number1 + state.number2;
+      break;
+    }
+    case "SUBTRACT" : {
+      state.result = state.number1 - state.number2;
+      break;
+    }
+    case "CLEAR" : {
+      state = initialState;
+    }
+    default: {
+      break;
     }
   }
 
-  return { type: action.type, result: sum, completed: true };
+  return {...state};
 }
 
 export default function SimpleCalculator () {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const [ops, dispatch] = useReducer(reducer, initialOps);
-  let [selectedNumbers, setSelectedNumbers] = useState([]);
-  
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
       <div>
         <h2>Number 1</h2>
         {numbers.map(number => (
-          <button key={number} onClick={ () => { 
-              if (selectedNumbers.length > 1) {
-                selectedNumbers = [];
-              }
-
-              selectedNumbers.push(number);
-              setSelectedNumbers(selectedNumbers);
-            }
-          }>
+          <button key={number} onClick={() => dispatch({ num: number, type: "NUMBER_1" })}>
             {number}
           </button>))}
       </div>
       <div>
         <h2>Number 2</h2>
         {numbers.map(number => (
-          <button key={number} onClick={ () => { 
-            if (selectedNumbers.length > 2) {
-              selectedNumbers = [];
-            }
-            selectedNumbers.push(number);
-            setSelectedNumbers(selectedNumbers);
-          }
-        }>
+          <button key={number} onClick={ () => dispatch({ num: number, type: "NUMBER_2"})}>
             {number}
           </button>))}
       </div>
       <div>
         <h2>Actions</h2>
-        <button onClick={ () => { dispatch ( { numbers: selectedNumbers, type: "ADD" }) }}>+</button>
-        <button onClick={ () => dispatch ({ numbers: selectedNumbers, type: "SUBSTRACT" })}>-</button>
-        <button onClick={ () => dispatch ({ numbers: selectedNumbers, type: "CLEAR" })} >c</button>
+        <button onClick={ () => dispatch({ type: "ADD"})}>+</button>
+        <button onClick={ () => dispatch({ type: "SUBTRACT" })}>-</button>
+        <button onClick={ () => dispatch({ type: "CLEAR" })}>c</button>
       </div>
-      <div><h2>Result:{ops.result}</h2></div>
+      <div><h2>Result:{state.result}</h2></div>
     </div>
   )
 }
